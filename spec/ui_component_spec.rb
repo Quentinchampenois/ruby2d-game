5ded8e544a1bfa47badf1ed1ba145d47e2a619d0
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-01-15 12:09:53 +0100

    add rspec

---
+++ spec/ui_component_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require '../ui_component'
+
+describe UiComponent do
+  let(:subject) { described_class }
+  let(:data) { { title: "fake data", x: 50, y: 50, width: 200, height: 200 } }
+
+  describe '#new' do
+    let(:subject) { described_class.new }
+
+    it 'creates empty array' do
+      expect(subject.ui_elements).to match([])
+    end
+  end
+
+  describe '#register' do
+    let(:subject) { described_class.new }
+    let(:get_idx) { false }
+
+    it 'store data in ui_elements' do
+      subject.register(data, get_idx)
+      expect(subject.ui_elements).to match([data])
+    end
+
+    it 'does not return last index' do
+
+      expect(subject.register(data, get_idx)).to be_nil
+    end
+
+    context "when get_idx is true" do
+      let(:get_idx) { true }
+
+      it 'store data in ui_elements' do
+        expect(subject.register(data, get_idx)).to eq(0)
+      end
+    end
+  end
+end
