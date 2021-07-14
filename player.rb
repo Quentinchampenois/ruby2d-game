e04072c0fa5ac6e5a3c2227d3db77484bf14d9af
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-07-14 15:49:02 +0200

    add ship png

--- player.rb
+++ player.rb
@@ -2,7 +2,11 @@ class Player
   attr_accessor :shape
 
   def initialize(square_size)
-    @shape = Square.new(x: (Window.width + square_size) / 2, y: (Window.height + square_size) - 100, size: square_size - 1,
-                        color: 'white')
+    @shape = Image.new(
+        'components/ship_1.png',
+        x: (Window.width + square_size) / 2, y: (Window.height + square_size) - 150,
+        width: 100, height: 100
+    )
+
   end
 end
